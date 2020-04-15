6f7de2850cfbd610dcc98d525808c8d86f43252e
Smart Coder <smartcoder415@gmail.com>
2020-04-15 11:09:29 -0400

    added LoadingContainer

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 
-import { Header } from './components/Header';
+import { Header } from './components';
 import Routes from './screens';
 
 const MainRoutes = () => {

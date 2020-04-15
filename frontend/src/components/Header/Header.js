2e0f1237d6f74238643e57a60a49bab36e896f23
Smart Coder <smartcoder415@gmail.com>
2020-04-15 11:01:58 -0400

    added Header

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar, Nav, Form } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Weather Forecast</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+      </Nav>
+    </Navbar>
+  );
+};
+
+export default Header;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
+
+import { Header } from './components/Header';
 import Routes from './screens';
 
 const MainRoutes = () => {
   return (
     <>
       <ToastContainer />
+      <Header />
       <div className="app-content">
         <Routes />
       </div>
